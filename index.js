8033f224f393db318609f6e7a521c6e4cc4de71b
Niks Hariman <nhariman@hotmail.com>
2023-06-18 22:09:11 -0500

    removed redundant code

--- index.js
+++ index.js
@@ -165,63 +165,6 @@ app.get('/api/users/:_id/logs/:limit?/:from?/:to?', async (req, res) => {
   });
 });
 
-// You can make a GET request to /api/users/:_id/logs to retrieve a full exercise log of any user.
-
-const getUserLogs = (userId, queries, done) => {
-  Exercise.find({
-    user_id: userId,
-  })
-    .limit(queries.limit)
-    .select('description duration date')
-    .exec((err, data) => {
-      if (err) return console.error(err);
-      done(null, data);
-    });
-};
-
-// app.get('/api/users/:_id/logs', (req, res, next) => {
-//   const getQueries = {
-//     from: req.query.from ? new Date(req.query.from) : null,
-//     to: req.query.to ? new Date(req.query.to) : null,
-//     limit: req.query.limit ? parseInt(req.query.limit) : null,
-//   };
-//   res.log = getUserLogs(req.params._id, getQueries, (err, data) => {
-//     if (err) {
-//       console.error(err);
-//       return;
-//     }
-//     console.log(data);
-//   });
-//   res.log.filter((item) => item.date.getTime() >= getQueries.from.getTime()
-//   && item.date.getTime() <= getQueries.to.getTime());
-//   res.user = findUserById(req.params._id, (err, data) => {
-//     if (err) {
-//       console.error(err);
-//       return;
-//     }
-//     console.log(data);
-//   });
-//   res.count = res.log.length;
-//   // can add from, to and limit parameters
-//   // from: yyyy-mm-dd
-//   // to: yyyy-mm-dd
-//   // limit: integer
-//   next();
-// }, (req, res) => {
-//   res.send({
-//     username: res.user.username,
-//     count: res.count,
-//     _id: res.user._id,
-//     log: res.log,
-//   });
-//   // returns user object with a count property representing
-//   // number of exercises that belong to that user
-//   // log array contains:
-//   // description: string
-//   // duration: number
-//   // date: Date
-// });
-
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log(`Your app is listening on port ${listener.address().port}`);
 });
